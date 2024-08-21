e59efae6bb295c1dab196cd6d8ccb81d38594d1d
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2024-08-21 15:22:33 -0700

    Go to file search

--- src/components/CodeBar.tsx
+++ src/components/CodeBar.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import Button from "./Button";
 import IconGitBranch from "./icons/GitBranch";
 import IconCode from "./icons/Code";
+import Input from "./Input";
+import IconSearch from "./icons/Search";
 export const CodeBar = () => {
+  const [fileQuery, setFileQuery] = React.useState("");
+
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row justify-between gap-4">
       <div className="flex flex-row space-x-2">
         <Button isDropdown>
           <IconGitBranch />
@@ -15,7 +19,12 @@ export const CodeBar = () => {
         </Button>
       </div>
       <div className="flex flex-row space-x-2">
-        <input placeholder="Search files" className="bg-none " />
+        <Input
+          leadingIcon={<IconSearch />}
+          placeholder="Go to file"
+          value={fileQuery}
+          onChange={(e) => setFileQuery(e.target.value)}
+        />
         <Button style="primary" isDropdown>
           <IconCode />
           Code
