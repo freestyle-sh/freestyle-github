16d3fe429c64e7a6cf0a4833a6f1585b15797544
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-21 15:11:42 -0700

    upgrades + background coloring

--- src/cloudstate/simple-repo.ts
+++ src/cloudstate/simple-repo.ts
@@ -1,83 +1,100 @@
 import { cloudstate } from "freestyle-sh";
 
 export interface RepoMetadata {
-    name: string;
-    description: string;
-    link: string;
-    starCount: number;
-    forkCount: number;
+  name: string;
+  description: string;
+  link: string;
+  starCount: number;
+  forkCount: number;
 }
 
 export type CodebaseMetadata = {
-    [path: string]: {
-        isDir: boolean;
-        isFile: boolean;
-        lastestCommitMessage: string
-        lastestCommitDate: number;
-    };
+  latestCommit: {
+    message: string;
+    date: number;
+    shortHash: string;
+  };
+  totalCommits: number;
+  files: {
+    [path: string]: FileMetadata;
+  };
+};
+
+export interface FileMetadata {
+    isDir: boolean;
+    isFile: boolean;
+    lastestCommitMessage: string;
+    lastestCommitDate: number;
 }
 
 @cloudstate
 export class SimpleRepo {
-    static readonly id = "simple-repo";
-    name = "Simple-Repo".toLowerCase();
-    description = "A simple repo that stores a simple codebase";
-    link = "https://www.freestyle.sh";
-    codebase = {
-        filename: "simple-repo.ts",
+  static readonly id = "simple-repo";
+  name = "Simple-Repo".toLowerCase();
+  description = "A simple repo that stores a simple codebase";
+  link = "https://www.freestyle.sh";
+  codebase = {
+    filename: "simple-repo.ts",
+  };
+  starCount = 0;
+  forkCount = 0;
+
+  getInfo() {
+    return {
+      name: this.name,
+      description: this.description,
+      link: this.link,
+      starCount: this.starCount,
+      forkCount: this.forkCount,
     };
-    starCount = 0;
-    forkCount = 0;
+  }
 
-    getInfo() {
-        return {
-            name: this.name,
-            description: this.description,
-            link: this.link,
-            starCount: this.starCount,
-            forkCount: this.forkCount,
-        };
-    }
-
-    getLatestCodebaseMetadata(path: string): CodebaseMetadata {
-        return {
-            "filename.ts": {
-                isDir: false,
-                isFile: true,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-            "src": {
-                isDir: true,
-                isFile: false,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-            "components": {
-                isDir: true,
-                isFile: false,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-            "icons": {
-                isDir: true,
-                isFile: false,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-            "layouts": {
-                isDir: true,
-                isFile: false,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-            "package.json": {
-                isDir: false,
-                isFile: true,
-                lastestCommitMessage: "Initial commit",
-                lastestCommitDate: Date.now(),
-            },
-        }
-    }
-
+  getLatestCodebaseMetadata(path: string): CodebaseMetadata {
+    return {
+      latestCommit: {
+        message: "Initial commit",
+        date: Date.now(),
+        shortHash: "123456",
+      },
+      totalCommits: 1,
+      files: {
+        "filename.ts": {
+          isDir: false,
+          isFile: true,
+          lastestCommitMessage: "Initial commit",
+          lastestCommitDate: Date.now(),
+        },
+        src: {
+          isDir: true,
+          isFile: false,
+          lastestCommitMessage: "Initial commitafgasdgsdg this one is super long and will not be allowed to go to two lines god damn you",
+          lastestCommitDate: Date.now(),
+        },
+        components: {
+          isDir: true,
+          isFile: false,
+          lastestCommitMessage: "Initial commit",
+          lastestCommitDate: Date.now(),
+        },
+        icons: {
+          isDir: true,
+          isFile: false,
+          lastestCommitMessage: "Initial commit",
+          lastestCommitDate: Date.now(),
+        },
+        layouts: {
+          isDir: true,
+          isFile: false,
+          lastestCommitMessage: "Initial commit",
+          lastestCommitDate: Date.now(),
+        },
+        "package.json": {
+          isDir: false,
+          isFile: true,
+          lastestCommitMessage: "Initial commit",
+          lastestCommitDate: Date.now(),
+        },
+      },
+    };
+  }
 }
--- src/layouts/Repo.tsx
+++ src/layouts/Repo.tsx
@@ -2,30 +2,45 @@ import type { CodebaseMetadata, RepoMetadata } from "../cloudstate/simple-repo";
 import { RepoBar } from "../components/RepoBar";
 import { RepoSidebar } from "../components/SideBar";
 import { CodeBar } from "../components/CodeBar";
-
+import { format } from "timeago.js";
+import { FileIcon, getIconForFile } from "../lib/icon-map";
+import { FileRow } from "../components/FileRow";
 // const { ThemeProvider } = await import('@primer/react');
 
 export function RepoLayout(props: {
   repoMetadata: RepoMetadata;
-  codeMetadata: CodebaseMetadata
+  codeMetadata: CodebaseMetadata;
 }) {
-  const { repoMetadata } = props;
+  const { repoMetadata, codeMetadata } = props;
 
   return (
     <div className="w-full flex flex-col">
       <RepoBar repoMetadata={repoMetadata} />
-      <div className="grid grid-cols-[2fr,1fr] w-full h-full px-8 py-4">
+      <div className="grid grid-cols-[2fr,1fr] w-full h-full px-12 py-4">
         <div className="pr-4">
           <CodeBar />
-          <div className="mt-4"/>
+          <div className="mt-4" />
           <div className="rounded-lg border border-[#30363d] overflow-hidden">
-            <div className="bg-[#161b22] p-2 border-b border-gray-700">
-            This is testing colors and what not
+            <div className="bg-[#161b22] p-2 border-b border-gray-700 flex flex-row justify-between items-center">
+              <div className="text-gray-400">
+                {codeMetadata.latestCommit.message}
+              </div>
+              <div className="text-xs text-gray-400">
+                {codeMetadata.latestCommit.shortHash} •{" "}
+                {format(codeMetadata.latestCommit.date)}
+              </div>
             </div>
-            <div className="px-4 py-2">
-              File 1
-            </div>
-
+            {Object.entries(codeMetadata.files).map((
+              [path, file],
+              index,
+              all,
+            ) => (
+              <FileRow
+                key={path}
+                fileMetadata={{ ...file, path }}
+                isLast={index !== all.length - 1}
+              />
+            ))}
           </div>
         </div>
         <div className="pl-4">
