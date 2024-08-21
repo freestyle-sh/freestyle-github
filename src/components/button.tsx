d8805ba44f953423f37f62ddddb26bbec57641d6
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2024-08-21 14:34:32 -0700

    fix btn styling

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -15,7 +15,7 @@ export default function Button({ onClick, style = "primary", children }: ButtonP
       className={`${
         style === "primary"
           ? "bg-[#1f883d] hover:bg-[#358e4e] text-white"
-          : "bg-gray-700 hover:bg-gray-600 active:opacity-80 border border-gray-600 text-white/75 py-1 px-4 text-xs font-medium"
+          : "bg-[#21262d] hover:bg-[#292e36] active:opacity-80 border border-[#30363d] text-white/75 py-1 px-4 text-xs font-medium"
       } rounded-lg flex flex-row gap-2 items-center justify-center transition duration-75`}
     >
       {children}
