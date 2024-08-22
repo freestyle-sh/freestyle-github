ade908728ad6266934948dd2c0f89208feba0e25
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-08-21 20:06:24 -0700

    git git

--- src/components/RepoBar.tsx
+++ src/components/RepoBar.tsx
@@ -3,6 +3,8 @@ import Button from "./Button";
 import CountIndicator from "./CountIndicator";
 import IconRepoForked from "./icons/Fork";
 import IconStar from "./icons/Star";
+import { emojiBlast } from "emoji-blast";
+
 
 export const RepoBar = (props: { repoMetadata: RepoMetadata }) => {
   const { repoMetadata } = props;
@@ -10,15 +12,33 @@ export const RepoBar = (props: { repoMetadata: RepoMetadata }) => {
     <div className="px-6 sm:px-12 pt-4">
       <div className="flex flex-row justify-between">
         <h1 className="dark:text-white text-xl font-semibold">
+          <a className=" hover:underline" href={
+           `/${repoMetadata.owner}/${repoMetadata.name}`
+          }>
           {repoMetadata.name}
+          </a>
         </h1>
         <div className="flex flex-row space-x-2">
-          <Button style="secondary" spacing="compact">
+          <Button style="secondary" spacing="compact" disabled
+            >
             <IconRepoForked />
             Fork
             <CountIndicator count={repoMetadata.forkCount} />
           </Button>
-          <Button style="secondary" spacing="compact">
+          <Button style="secondary" spacing="compact" onClick={(evt)=>{
+            emojiBlast(
+              { 
+                
+                position: { x: evt.clientX, y: evt.clientY },
+                emojiCount: 100,
+                emojis: [
+                  "⭐"
+                ],
+                
+              },
+              
+            )
+          }}>
             <IconStar starred={false} />
             Star
             <CountIndicator count={repoMetadata.starCount} />
