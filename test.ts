066ac75ee84dcfadc4c2510095b478fad22aa657
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2024-10-17 14:19:57 -0700

    zenfs and isomorphic-git test

---
+++ test.ts
@@ -0,0 +1,37 @@
+import { fs } from "npm:@zenfs/core";
+import { getOrMountRepo } from "./src/cloudstate/simple-repo.ts";
+import git from "npm:isomorphic-git";
+
+await getOrMountRepo("test");
+
+await git.init({
+  fs,
+  dir: "/test",
+});
+
+await git.branch({
+  fs,
+  dir: "/test",
+  ref: "main",
+  checkout: true,
+});
+
+fs.writeFileSync("/test/test.txt", "test");
+
+await git.add({
+  fs,
+  dir: "/test",
+  filepath: "test.txt",
+});
+
+{
+  for (const prefix of fs.readdirSync(`${"test"}/.git/objects`)) {
+    for (const file of fs.readdirSync(`${"test"}/.git/objects/${prefix}`)) {
+      console.log(
+        prefix,
+        file,
+        fs.readFileSync(`${"test"}/.git/objects/${prefix}/${file}`).toString()
+      );
+    }
+  }
+}
